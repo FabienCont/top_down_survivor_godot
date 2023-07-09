fd6e6feeb2ddb5fc3fdc724aaf19883da0a61bed
Fabien Contoli <contoli.fabien@gmail.com>
2023-07-09 13:01:29 +0200

    ajout resources Player

---
+++ scripts/resources/Stats.gd
@@ -0,0 +1,12 @@
+extends Resource
+
+class_name Stats
+
+var life: float = 10
+var max_life: float = 10
+var move_speed: float = 10
+var attack_speed: float = 10
+var level: int = 1
+var damage: int = 1
+var visibility: int = 6
+var collect_distance: int = 6
